48fb0841e4455d61ea1787d4c67682aea4d95e15
Matheus <Matheus@MATHEUS-PC>
2015-11-10 14:08:54 -0200

    Bug na leitura completamente sem sentido (\r do além?) corrigido. Agora é esperado que o programa funcione em todas as máquinas.

--- onibus-base/src/pucrs/progoo/reader/Leitura.java
+++ onibus-base/src/pucrs/progoo/reader/Leitura.java
@@ -1,6 +1,4 @@
 package pucrs.progoo.reader;
-
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
@@ -19,11 +17,6 @@ import pucrs.progoo.model.Parada;
 
 import java.util.ArrayList;
 
-
-
-
-
-
 public class Leitura{
 	
 	//testes
@@ -31,9 +24,7 @@ public class Leitura{
 		System.out.println("teste");
 		try{
 			Map<String, Linha> dic = geraLinhas();
-					
 			System.out.println(dic.size());
-			
 			for(String key: dic.keySet()){
 				System.out.println(dic.get(key));
 				Linha linha = dic.get(key);
@@ -51,14 +42,13 @@ public class Leitura{
 	//
 
 	private static Map<String, Linha> preparaLinha() throws IOException{
-		
 		Map<String, Linha> dic = new HashMap<String, Linha>();		
 		Path path = Paths.get("linhas.csv");
 		
 		try(Scanner sc = new Scanner(Files.newBufferedReader(path, Charset.forName("utf-8")))){
 			sc.useDelimiter("[;\n]"); // separadores: ; e nova linha
 			String header = sc.nextLine(); // pula cabecalho
-			
+		
 			while(sc.hasNext()){
 				String id = sc.next();
 				String nome = sc.next();
@@ -66,8 +56,10 @@ public class Leitura{
 				String codigo = sc.next();
 				codigo = codigo.substring(1, codigo.length()-1);
 				String tipo = sc.next();
-				tipo = tipo.substring(1, tipo.length()-2); //neste caso, o formato de entrada no arquivo é, por exemplo, " 'O'' "
-
+				//tipo = tipo.substring(1, tipo.length()-2); //neste caso, o formato de entrada no arquivo é, por exemplo, " 'O'' "
+				//XXX teste para ver se o bug de leitura nos outros computadores é resolvido
+				tipo = Character.toString(tipo.charAt(1));
+			
 				if(tipo.equals("O")){
 					char tipoChar = tipo.charAt(0);
 					Linha linha = new Linha(id, nome, codigo, tipoChar);
@@ -75,15 +67,16 @@ public class Leitura{
 				}
 				
 			}
-		return dic;
+			return dic;
 		}
 	}
+		
+
 	
 	
 	private static void preparaCoordenada(Map<String, Linha> dic) throws IOException{
-	
 		Path path = Paths.get("coordenadas.csv");
-				
+		
 		try(Scanner sc = new Scanner(Files.newBufferedReader(path, Charset.forName("utf-8")))){
 			sc.useDelimiter("[;\n]"); // separadores: ; e nova linha
 			String header = sc.nextLine(); // pula cabecalho
@@ -99,16 +92,9 @@ public class Leitura{
 				longitude = longitude.replaceAll(",", ".");
 				double longitudeDouble = Double.parseDouble(longitude);
 				
-				
 				String idLinha = sc.next();
-				
-				/*
-				 *A substring abaixo é feita porque o programa lê o idLinha como, por exemplo, "127816\r" e não permite que essa chave seja usada no hash map.
-				 *A substring elimina esse "\r" da String.
-				 */
-				idLinha = idLinha.substring(0, idLinha.length()-1);
-				
 				Linha linha = dic.get(idLinha);
+				
 				if(linha != null){
 					Coordenada novaCoordenada = new Coordenada(latitudeDouble, longitudeDouble);
 					ArrayList<Coordenada> lista = linha.getCoordenadas();
@@ -118,7 +104,6 @@ public class Leitura{
 			}
 		}
 	}
-
 	
 	public static Map<String, Parada> preparaParada() throws IOException{
 		Map<String, Parada> dic = new HashMap<>();
@@ -155,47 +140,39 @@ public class Leitura{
 	
 
 	private static Map<String, ArrayList<String>> preparaParadaLinha() throws IOException{
-
 		Map<String, ArrayList<String>> dic = new HashMap<>();
-		
 		Path path = Paths.get("paradalinha.csv");
-				
+		
 		try(Scanner sc = new Scanner(Files.newBufferedReader(path, Charset.forName("utf-8")))){
 			sc.useDelimiter("[;\n]"); // separadores: ; e nova linha
 			String header = sc.nextLine(); // pula cabecalho
-			
 			String idLinha = sc.next(), novoIdLinha = null;
 			
 			while(sc.hasNext()){
-
 				ArrayList<String> lista = new ArrayList<>();
 				
 				while(sc.hasNext()){
-					
 					String idParada = sc.next();
-					idParada = idParada.substring(0, idParada.length()-1);
 					lista.add(idParada);
+					
 					if(sc.hasNext()){
 						novoIdLinha = sc.next();
 					}
-					
 					if(!novoIdLinha.equals(idLinha)){
 						dic.put(idLinha, lista);
 						idLinha = novoIdLinha;
 						break;
 					}
-					
 					if(!sc.hasNext()){
 						dic.put(idLinha, lista);
 					}
 				}
 			}
-		return dic;
+			return dic;
 		}
 	}
-	
+					
 	private static void insereParada(Map<String, ArrayList<String>> paradaLinha, Map<String, Parada> paradas, Map<String, Linha> linhas) throws IOException{
-		
 		for(String idLinha: paradaLinha.keySet()){
 			Linha linha = linhas.get(idLinha);
 			ArrayList<String> listaParadaLinha = paradaLinha.get(idLinha);
@@ -207,15 +184,14 @@ public class Leitura{
 					Parada parada = paradas.get(idParada);
 					dicParada.put(idParada, parada);
 				}
-				
 			}
 			linha.setParadas(dicParada);
 		}
 	}
+		
 	
 
 	public static Map<String, Linha> geraLinhas() throws IOException{
-		
 		Map<String, Linha> dic = preparaLinha();
 		preparaCoordenada(dic);
 		Map<String, ArrayList<String>> paradaLinha = preparaParadaLinha();
@@ -223,8 +199,5 @@ public class Leitura{
 		insereParada(paradaLinha, parada, dic);
 		
 		return dic;
-	}
-	
-	
+	}	
 }
-
